15f7e8ddc9e614aeeadf0c64e18b750f36313770
aaronsaunders <ams@bio.aau.dk>
2013-11-19 14:15:53 +0100

    combined otu name parsing and fasta fetching (with biopython dict so fast with big files)

--- seqIDs_from_otus.py
+++ seqIDs_from_otus.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
  
 """
-Usage: seqIDs_from_otus.py [id_list] [seqs_otus.txt]
+Usage: seqIDs_from_otus.py [id_list] [seqs_otus.txt] [seqs.fna] [outfile.fna]
 
-returns a list of seqnames to stdout
+takes a list of OTU IDs and the qiime otu mapping file
+
+returns a list of seqnames to file (filename last argument)
 
 Copyright (C) 2013 Aaron Saunders ams@bio.aau.dk
 
 """
 import sys
+from Bio import SeqIO
 
 args = sys.argv[1:]
 
 if len(args) == 2:
     idfname = args[0]
     otufname = args[1]
+    seqsfname = args[2]
+    outfname = args[3]
 else:
     sys.exit(__doc__)
 
@@ -28,12 +33,19 @@ with open(otufname, 'r') as fh:
         line = line.strip()
         otuid = line.split('\t')[0]
         if otuid in otuids:
-            seqnames.extend( line.split('\t')[1:] )
+            otus = line.split('\t')[1:]
+            for otu in otus:
+                seqnames.append(otu)
             
-result = '\n'.join(seqnames)
+fasta_dict = SeqIO.index(seqsfname, "fasta")
 
-sys.stdout.write(result)    
+recstokeep = [ fasta_dict[seqid] for seqid in seqnames ]
 
+with open(outfname, 'w') as outfh:
+    SeqIO.write(recstokeep, outfh, "fasta")
 
-
+handle = open(outfname, 'w')
+for record in recstokeep:
+    handle.write(">%s\n%s\n" % (record.description, record.seq))
+handle.close()
 
